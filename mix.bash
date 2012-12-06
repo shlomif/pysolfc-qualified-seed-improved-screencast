90951c82cb5f6ce64f587bc35ed37f3bbc019f29
Shlomi Fish <shlomif@shlomifish.org>
2012-12-07 00:01:16 +0200

    Convert mix.bash to a more meaningful filename.

--- mix.bash
+++ mix.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i unreal_dm_-_Reach_Out-trimmed-and-faded-out.wav -i pysolfc-qsi.ogv -map 0:0 -map 1:1 -shortest -c:a libvorbis -q:a 7 -c:v copy out.ogv
+ffmpeg -i unreal_dm_-_Reach_Out-trimmed-and-faded-out.wav -i pysolfc-qsi.ogv -map 0:0 -map 1:1 -shortest -c:a libvorbis -q:a 7 -c:v copy pysolfc-qsi-with-audio.ogv
