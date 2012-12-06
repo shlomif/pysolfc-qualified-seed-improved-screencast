a8235d950c47346c9af62743bebf1ba67e76e32d
Shlomi Fish <shlomif@shlomifish.org>
2012-12-06 22:37:55 +0200

    Final mix.bash.

--- mix.bash
+++ mix.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i unreal_dm_-_Reach_Out-trimmed-and-faded-out.wav -i pysolfc-qsi.ogv -acodec copy -vcodec copy output.ogv -map 0.0 -map 1.0
+ffmpeg -i unreal_dm_-_Reach_Out-trimmed-and-faded-out.wav -i pysolfc-qsi.ogv -map 0:0 -map 1:1 -shortest -c:a libvorbis -q:a 7 -c:v copy out.ogv
